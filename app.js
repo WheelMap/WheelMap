ab8d69ba013b743b82858ac6db0aff7f35545322
Seyeon-Park <113195498+seyeonparkk@users.noreply.github.com>
2023-09-10 23:07:08 +0900

    임시용 푸시

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const mysql = require('mysql2');
 
-
 const db = mysql.createConnection({
-  host: 'localhost',         
-  user: 'root',              
-  password: '0000',          
-  database: 'wm'             
+  host: 'localhost',
+  user: 'root',
+  password: '0000',
+  database: 'wm'
 });
+
+
 
 
 db.connect((err) => {
@@ -15,7 +16,6 @@ db.connect((err) => {
     throw err;
   }
   console.log('MySQL 데이터베이스에 연결되었습니다.');
-
 
   db.query(`
     CREATE TABLE IF NOT EXISTS user_info (
@@ -27,26 +27,31 @@ db.connect((err) => {
   `, (err, result) => {
     if (err) {
       console.error('테이블 생성 중 오류 발생:', err);
-      throw err;
+      db.end(); // 에러 발생 시 연결 종료
+      return;
     }
     console.log('user_info 테이블이 생성되었습니다.');
 
-   
     const userInfo = {
       name: '카카오톡에서 받은 이름',
       email: '카카오톡에서 받은 이메일',
       profile_picture_url: '프로필 사진 링크 URL'
     };
-
-    
+    app.get('/search/:name',function(request, response){   
+      //var body = request.body;
+  
+      
+      console.log(request.params.name);
+       //input으로 받은 menu 값 출력 확인
+   });
     db.query('INSERT INTO user_info SET ?', userInfo, (err, result) => {
       if (err) {
         console.error('데이터 저장 중 오류 발생:', err);
-        throw err;
+        db.end(); // 에러 발생 시 연결 종료
+        return;
       }
       console.log('데이터가 성공적으로 저장되었습니다.');
 
-     
       db.end((err) => {
         if (err) {
           console.error('MySQL 연결 종료 오류:', err);
