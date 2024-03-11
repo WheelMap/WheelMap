b0f6c704fdf3fede9f5c3e79c81e6c029f7b2067
seonyo <jsy6535@naver.com>
2024-03-12 00:24:20 +0900

    :fire:[Remove] index.js 필요없는 코드 제거

--- JS/index.js
+++ JS/index.js
@@ -1,7 +1,6 @@
-var map;
-var latitude;
-var longitude;
-
+let map;
+let latitude;
+let longitude;
 let menuOpen = false;
 
 function toggleMenu() {
@@ -30,16 +29,15 @@ function toggleMenuIcon(element) {
 function getUserLocation() {
     return new Promise(function (resolve, reject) {
         navigator.geolocation.getCurrentPosition(function (position) {
-            // latitude = position.coords.latitude;
-            // longitude = position.coords.longitude;
-            latitude = 37.54551031331041;
-            longitude = 126.9526828436507;
+            latitude = position.coords.latitude;
+            longitude = position.coords.longitude;
             resolve();
         }, function (error) {
             reject(error);
         });
     });
 }
+
 // 페이지가 로딩이 된 후 호출하는 함수입니다.
 async function initializeMap() {
     try {
@@ -48,13 +46,13 @@ async function initializeMap() {
         var headers = {};
         headers["appKey"] = "q1hz24YqUC7g84TRhAW3v8a52xq51B3472o9tPeF";
         // map 생성
-        // Tmapv3.Map을 이용하여, 지도가 들어갈 div, 넓이, 높이를 설정합니다.
         map = new Tmapv2.Map("map", { // 지도가 생성될 div
             center: new Tmapv2.LatLng(latitude, longitude),
             width: "100%",   // 지도의 넓이
             height: "95%",   // 지도의 높이
             zoom: 16   // 지도 줌레벨
         });
+
         // 시작 심볼 찍기
         marker_s = new Tmapv2.Marker(
             {
@@ -71,16 +69,9 @@ async function initializeMap() {
 
 async function initTmap() {
     try {
-        await getUserLocation();
         initializeMap();
     } catch (error) {
         console.log("Error");
     }
-}
-function detectLocationChange({ coords, timestamp }) {
-    // latitude = coords.latitude;   // 위도
-    // longitude = coords.longitude; // 경도
-    latitude = 37.54551031331041;
-    longitude = 126.9526828436507;
 }
 
